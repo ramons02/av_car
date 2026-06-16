9a670ed8a0e473f66239df2fc4280a7dde671f50
Ramon Silva <ramonsillva70@gmail.com>
2026-06-16 08:59:38 -0300

    Arquivo final

--- src/main/java/br/edu/senai/fatesg/avcar/AvCarApplication.java
+++ src/main/java/br/edu/senai/fatesg/avcar/AvCarApplication.java
@@ -1,6 +1,5 @@
 package br.edu.senai.fatesg.avcar;
 
-import br.edu.senai.fatesg.avcar.swing.views.MainView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -22,7 +21,9 @@ public class AvCarApplication implements ApplicationRunner {
         // --- INICIALIZAÇÃO DO TEMA DARK PREMIUM ---
         try {
             com.formdev.flatlaf.FlatDarkLaf.setup();
-            javax.swing.UIManager.put("defaultFont", new java.awt.Font("Segoe UI", java.awt.Font.PLAIN, 14));
+            // CORREÇÃO CROSS-PLATFORM: Usa a fonte nativa do S.O. (Ex: Ubuntu no Linux, Segoe no Win) 
+            // no tamanho 14, garantindo que botões não fiquem cortados.
+            javax.swing.UIManager.put("defaultFont", javax.swing.UIManager.getFont("Label.font").deriveFont(14f));
             javax.swing.UIManager.put("TextComponent.arc", 15);
             javax.swing.UIManager.put("Button.arc", 15);
             javax.swing.UIManager.put("Component.arc", 15);
@@ -34,8 +35,17 @@ public class AvCarApplication implements ApplicationRunner {
             javax.swing.UIManager.put("TabbedPane.hoverColor", new java.awt.Color(70, 73, 75));
             javax.swing.UIManager.put("TabbedPane.selectedBackground", new java.awt.Color(43, 43, 43));
             
-            // --- ESTILIZAÇÃO DAS TABELAS (ZEBRADO) ---
+            // --- ESTILIZAÇÃO DAS TABELAS (ZEBRADO E HEADERS) ---
             javax.swing.UIManager.put("Table.alternateRowColor", new java.awt.Color(60, 63, 65));
+            javax.swing.UIManager.put("TableHeader.font", javax.swing.UIManager.getFont("Label.font").deriveFont(java.awt.Font.BOLD, 13f));
+            javax.swing.UIManager.put("TableHeader.background", new java.awt.Color(45, 48, 50));
+            javax.swing.UIManager.put("TableHeader.foreground", new java.awt.Color(180, 185, 190));
+            javax.swing.UIManager.put("TableHeader.separatorColor", new java.awt.Color(60, 63, 65));
+            javax.swing.UIManager.put("TableHeader.bottomSeparatorColor", new java.awt.Color(80, 130, 180));
+            
+            // --- ESTILIZAÇÃO DE BOTÕES HOVER ---
+            javax.swing.UIManager.put("Button.hoverBackground", new java.awt.Color(70, 73, 75));
+            javax.swing.UIManager.put("Button.pressedBackground", new java.awt.Color(80, 85, 90));
         } catch (Exception ignored) {}
         
         SpringApplication.run(AvCarApplication.class, args);
@@ -44,11 +54,8 @@ public class AvCarApplication implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) {
         SwingUtilities.invokeLater(() -> {
-            // Abre o painel antigo para não perdermos a referência
-            ctx.getBean(MainView.class).setVisible(true);
-            
             // Abre o painel NOVO lado a lado, rodando com o Banco de Dados injetado!
-            ctx.getBean(br.edu.senai.fatesg.avcar.swing.views.telas.TelaPrincipalGUI.class).setVisible(true);
+            ctx.getBean(br.edu.senai.fatesg.avcar.swing.views.TelaPrincipalGUI.class).setVisible(true);
         });
     }
 }
